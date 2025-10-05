5edbecb9b621dbb4106fc2f4a97708670c96f6d6
Monty Milan Biswal <montymilanbiswal27@gmail.com>
2025-10-05 21:42:58 +0000

    Tell me everything that I need to configure in environment variables in

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-5878346757-4e20a",
-  "appId": "1:91261280809:web:2d7e13e8f08e794ac13238",
-  "apiKey": "AIzaSyCmLWmSy_RHxlzSg1P6kGcDzD_VouVZiQk",
-  "authDomain": "studio-5878346757-4e20a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "91261280809"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
