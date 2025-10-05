e61d918746ad79ab68a28179e8e15ceb2a4c941b
Monty Milan Biswal <montymilanbiswal27@gmail.com>
2025-10-05 17:30:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5878346757-4e20a",
+  "appId": "1:91261280809:web:2d7e13e8f08e794ac13238",
+  "apiKey": "AIzaSyCmLWmSy_RHxlzSg1P6kGcDzD_VouVZiQk",
+  "authDomain": "studio-5878346757-4e20a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "91261280809"
+};
